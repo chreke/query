7543b1261409864014a215754fe8c54030792090
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2025-02-17 06:42:21 +0100

    Update query module

--- query.py
+++ query.py
@@ -1,3 +1,5 @@
+import sqlite3
+import itertools
 
 class Col:
     def __init__(self, name):
@@ -6,10 +8,18 @@ class Col:
     def __str__(self):
         return self.name
 
-class Eq:
+class BinaryOp:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+
+class Eq(BinaryOp):
+    pass
+
+class Gt(BinaryOp):
+    pass
+
 
 class Query:
     def __init__(self, table):
@@ -19,26 +29,37 @@ class Query:
 
     def select(self, cols):
         self.projections = cols
+        return self
 
     def where(self, op):
         self.selections.append(op)
+        return self
 
     def join(self, table):
         self.tables.append(table)
+        return self
+
+    def __str__(self):
+        return (
+            f"Tables: {self.tables}\n"
+            f"Projections: {self.projections}\n"
+            f"Selections: {self.selections}\n")
 
 
 def _to_sql(term):
     match term:
-        case int():
-            return str(term)
-        case str():
-            return f"'{term}'"
         case Col(name=name):
             return name
         case Eq(a=a, b=b):
             return _to_sql(a) + " = " + _to_sql(b)
-        case other:
-            raise Exception(f"Unexpected operator: {other}")
+        case Gt(a=a, b=b):
+            return _to_sql(a) + " > " + _to_sql(b)
+        case str():
+            return f"'{term}'"
+        case int():
+            return str(term)
+        case _:
+            raise Exception(f"Unexpected {term}")
         
 
 def sql(query):
@@ -47,3 +68,46 @@ def sql(query):
     selections = " AND ".join(_to_sql(s) for s in query.selections)
     where = f"WHERE {selections}" if selections else ""
     return f"SELECT ({projections}) FROM {tables} {where}".strip()
+
+
+## ADAPTERS
+
+def merge_dicts(dicts):
+    merged = {}
+    for d in dicts:
+        merged.update(d)
+    return merged
+
+class Memory:
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def _compute(cls, term, row):
+        match term:
+            case Col(name=name):
+                return row[name]
+            case Eq(a=a, b=b):
+                return cls._compute(a, row) == cls._compute(b, row)
+            case Gt(a=a, b=b):
+                return cls._compute(a, row) > cls._compute(b, row)
+            case other:
+                return other
+
+    @classmethod
+    def _check_row(cls, row, selections):
+        return all(cls._compute(s, row) for s in selections)
+
+    def run(self, query):
+        tables = [self.data[t] for t in query.tables]
+        rows = (merge_dicts(t) for t in itertools.product(*tables))
+        return [r for r in rows if self._check_row(r, query.selections)]
+
+class SQLite:
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+
+    def run(self, query):
+        cursor = self.connection.cursor()
+        result = cursor.execute(query)
+        return result.fetchall()
